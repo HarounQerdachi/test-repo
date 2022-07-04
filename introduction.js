11c75409922daf517963f241f22c9074315d2391
Haroun Qerdachi <haroun.qerdachi12@gmail.com>
2022-07-04 15:21:28 +0100

    testing

---
+++ introduction.js
@@ -0,0 +1,29 @@
+console.log('Welcome to 30DaysOfJavaScript')
+
+// Checking Data Types
+
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+
+/*
+A valid JavaScript variable name must follow the following rules:
+
+-A JavaScript variable name should not begin with a number.
+-A JavaScript variable name does not allow special characters except dollar sign and underscore.
+-A JavaScript variable name follows a camelCase convention.
+-A JavaScript variable name should not have space between words.
+*/
+
+// Declaring different variables of different data types
+let firstName = 'Haroun' // first name of a person
+let lastName = 'Qerdachi' // last name of a person
+let country = 'Morocco' // country
+let city = 'Rabat' // capital city
+let age = 19 // age in years
+let isMarried = false
+
+console.log(firstName, lastName, country, city, age, isMarried)
